05b84cc04fc79d72f804f6251a2904af7576de59
makeitvsolo <bye.entv@gmail.com>
2023-09-08 15:37:00 +0300

    refactor: MatchScore tests

--- src/test/java/com/makeitvsolo/escoreboard/model/scoring/matchscore/MatchScoreTests.java
+++ src/test/java/com/makeitvsolo/escoreboard/model/scoring/matchscore/MatchScoreTests.java
@@ -2,9 +2,7 @@ package com.makeitvsolo.escoreboard.model.scoring.matchscore;
 
 import com.makeitvsolo.escoreboard.model.scoring.PlayerNumber;
 import com.makeitvsolo.escoreboard.model.scoring.ScoreState;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.stream.IntStream;
 
@@ -21,34 +19,51 @@ public class MatchScoreTests {
         Assertions.assertEquals(0, matchScore.totalPointsOf(PlayerNumber.Two));
     }
 
-    @Test
-    @DisplayName("player wins when he earns `2` points; 2:0")
-    public void twoZero() {
-        matchScore = MatchScore.initial();
+    @Nested
+    @DisplayName("while no player has `2` points")
+    public class WhileNoPlayerHasSixPoints {
 
-        IntStream.range(0, 1)
-                .forEach((round) -> {
-                    Assertions.assertEquals(ScoreState.UnknownWinner, matchScore.pointFor(PlayerNumber.One));
-                });
+        @BeforeEach
+        public void beforeEach() {
+            matchScore = MatchScore.initial();
+        }
 
-        Assertions.assertEquals(ScoreState.PlayerOneWin, matchScore.pointFor(PlayerNumber.One));
-        Assertions.assertEquals(2, matchScore.totalPointsOf(PlayerNumber.One));
-        Assertions.assertEquals(0, matchScore.totalPointsOf(PlayerNumber.Two));
-    }
+        @Test
+        @DisplayName("players earn points and winner is unknown")
+        public void playerEarnPointsAndWinnerIsUnknown() {
+            IntStream.range(0, 1)
+                    .forEach((round) -> {
+                        Assertions.assertEquals(ScoreState.UnknownWinner, matchScore.pointFor(PlayerNumber.One));
+                        Assertions.assertEquals(ScoreState.UnknownWinner, matchScore.pointFor(PlayerNumber.Two));
+                    });
+        }
 
-    @Test
-    @DisplayName("player wins when he earns `2` points; 2:1")
-    public void twoOne() {
-        matchScore = MatchScore.initial();
+        @Nested
+        @DisplayName("when some player earn `2` points")
+        public class WhenPlayerEarnTwoPointsHeIsWinner {
 
-        IntStream.range(0, 1)
-                .forEach((round) -> {
-                    Assertions.assertEquals(ScoreState.UnknownWinner, matchScore.pointFor(PlayerNumber.One));
-                    Assertions.assertEquals(ScoreState.UnknownWinner, matchScore.pointFor(PlayerNumber.Two));
-                });
+            @BeforeEach
+            public void beforeEach() {
+                IntStream.range(0, 1)
+                        .forEach((round) -> {
+                            matchScore.pointFor(PlayerNumber.One);
+                            matchScore.pointFor(PlayerNumber.Two);
+                        });
+            }
 
-        Assertions.assertEquals(ScoreState.PlayerOneWin, matchScore.pointFor(PlayerNumber.One));
-        Assertions.assertEquals(2, matchScore.totalPointsOf(PlayerNumber.One));
-        Assertions.assertEquals(1, matchScore.totalPointsOf(PlayerNumber.Two));
+            @Test
+            @DisplayName("winner is player one if he earn `2` points")
+            public void winnerIsPlayerOneIfHeEarnTwoPoints() {
+                Assertions.assertEquals(ScoreState.PlayerOneWin, matchScore.pointFor(PlayerNumber.One));
+                Assertions.assertEquals(2, matchScore.totalPointsOf(PlayerNumber.One));
+            }
+
+            @Test
+            @DisplayName("winner is player two if he earn `2` points")
+            public void winnerIsPlayerTwoIfHeEarnTwoPoints() {
+                Assertions.assertEquals(ScoreState.PlayerTwoWin, matchScore.pointFor(PlayerNumber.Two));
+                Assertions.assertEquals(2, matchScore.totalPointsOf(PlayerNumber.Two));
+            }
+        }
     }
 }
